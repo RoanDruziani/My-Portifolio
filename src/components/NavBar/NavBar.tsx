71551cbd4fee21d10da357a078ecca666214a69a
Roan Druziani <rddruziani@gmail.com>
2025-11-27 01:21:00 -0300

    init: create Navbar component

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,29 @@
+import { AppBar, MenuItem, Toolbar, styled } from "@mui/material"
+
+const NavBar = () => {
+
+    const StyledToolbar = styled(Toolbar)(() => ({
+        display:"flex",
+        justifyContent: "space-evenly"
+    }))
+
+    return (
+        <>
+            <AppBar position="absolute">
+                <StyledToolbar>
+                    <MenuItem>
+                        About
+                    </MenuItem>
+                    <MenuItem>
+                        Skils
+                    </MenuItem>
+                    <MenuItem>
+                        Projects
+                    </MenuItem>
+                </StyledToolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default NavBar
